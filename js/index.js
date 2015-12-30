d3d34b43c59bd5391b1d83393204ec3714d23ab3
Rowell Heria <owieh@me.com>
2015-12-30 23:55:18 +0000

    Now generates the search url. Error window appears if no search term is provided.

--- js/index.js
+++ js/index.js
@@ -30,6 +30,33 @@ function closeModal() {
   });
 }
 
+function displayNoResult() {
+  $('.splash-message').text('No result for \'' + getSearchTerm() + '\'!');
+}
+
+function generateSearchUrl(term, country, entity) {
+  const baseUrl = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsSearch?term=<%TERM%>&country=<%COUNTRY%>&entity=<%ENTITY%>';
+  var url = baseUrl.replace('<%TERM%>', getCleanSearchTerm());
+  url = url.replace('<%COUNTRY%>', getCountry());
+  return url.replace('<%ENTITY%>', getMediaType());
+}
+
+function getSearchTerm() {
+  return $('input[name="search-term"]').val().trim();
+}
+
+function getCleanSearchTerm() {
+  return encodeURIComponent(getSearchTerm());
+}
+
+function getCountry() {
+  return $('select[name="country"]').val();
+}
+
+function getMediaType() {
+  return $('.media-types input[type="radio"]:checked').val();
+}
+
 hideScrollbar();
 
 $('.pill-button-container label').on('click', function() {
@@ -49,4 +76,15 @@ $('.search-modal-overlay').on('click', function() {
 
 $('.cancel-search').on('click', function() {
   closeModal();
+});
+
+$('.search-artwork').on('click', function(ev) {
+  ev.preventDefault();
+  if (getSearchTerm().length > 0) {
+    displayNoResult();
+    generateSearchUrl();
+  }
+  else {
+    displayError('Search term can\'t be empty.', 'Please provide a search term and try again.');
+  }
 });
